a834f8445689cca1dbedad098da7e418112f0458
Raam Dev <raam@raamdev.com>
2014-03-14 15:46:57 -0400

    update-wp-theme-from-github.sh: Only update when different

--- update-wp-theme-from-github.sh
+++ update-wp-theme-from-github.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 # -------------------------------------------------------------------
 #
-# This script updates a WordPress theme with the latest code from 
+# This script updates a WordPress theme with the latest code from
 # GitHub. You can set it to run as a cronjob to automatically update
 # a WordPress theme with the latest code from the master branch (or
 # any branch that you want).
 #
 # Be sure to chmod +x this file so that it can be executed via Cron.
-# You should also keep this script outside your main web root so
-# that it's not accessible to the public.
 #
 # Example cronjob to run this script every 30 minutes:
 # 0,30 * * * * /home/independ/update-wp-theme-from-github.sh
@@ -23,11 +21,30 @@ GITHUB_ZIP_FILENAME="master.zip" # The name of the zip file that GitHub gives us
 GITHUB_ZIP_EXTRACTED_DIR_NAME="independent-publisher-master" # The name of the directory the GitHub zip file is extracted to
 CHOWN_USER="independ" # The name of the user who should own these files
 CHOWN_GROUP="independ" # The name of the group who should own these files
+GITHUB_ZIP_FILENAME_LATEST="master.zip.latest"
 
 cd $THEMES_DIR
-rm -rf $THEME_NAME
 /usr/bin/curl -o $GITHUB_ZIP_FILENAME -k -L $GITHUB_ZIP_URL
-/usr/bin/unzip master.zip
-mv $GITHUB_ZIP_EXTRACTED_DIR_NAME $THEME_NAME
-rm -rf master.zip
-chown -R $CHOWN_USER:$CHOWN_GROUP $THEMES_DIR/$THEME_NAME
+
+if [ -e $THEMES_DIR/$GITHUB_ZIP_FILENAME_LATEST ]; then
+        if diff $GITHUB_ZIP_FILENAME $GITHUB_ZIP_FILENAME_LATEST >/dev/null ; then
+                echo "Remote and local are identical; skipping update..."
+                exit # already have latest code; not need to update
+        else
+                echo "Remote is different than local; updating..."
+                rm -rf $THEME_NAME
+                /usr/bin/unzip $GITHUB_ZIP_FILENAME
+                mv $GITHUB_ZIP_EXTRACTED_DIR_NAME $THEME_NAME
+                mv $GITHUB_ZIP_FILENAME $GITHUB_ZIP_FILENAME_LATEST
+                chown -R $CHOWN_USER:$CHOWN_GROUP $THEMES_DIR/$GITHUB_ZIP_FILENAME_LATEST
+                chown -R $CHOWN_USER:$CHOWN_GROUP $THEMES_DIR/$THEME_NAME
+        fi
+else
+		echo "Fetching remote code for local installation..."
+        rm -rf $THEME_NAME
+        /usr/bin/unzip $GITHUB_ZIP_FILENAME
+        mv $GITHUB_ZIP_EXTRACTED_DIR_NAME $THEME_NAME
+        mv $GITHUB_ZIP_FILENAME $GITHUB_ZIP_FILENAME_LATEST
+        chown -R $CHOWN_USER:$CHOWN_GROUP $THEMES_DIR/$GITHUB_ZIP_FILENAME_LATEST
+        chown -R $CHOWN_USER:$CHOWN_GROUP $THEMES_DIR/$THEME_NAME
+fi
